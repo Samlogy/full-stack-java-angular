59c19a3a688e3711eb521257d024593707d752c5
samlogy <aceportgasd235@gmail.com>
2024-08-03 07:11:52 +0100

    angular conf add: angular routing

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { AuthConfig, OAuthService, provideOAuthClient } from 'angular-oauth2-oidc';
+import {
+  AuthConfig,
+  OAuthService,
+  provideOAuthClient,
+} from 'angular-oauth2-oidc';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'http://localhost:8180/realms/my-test-realm',
-  tokenEndpoint: 'http://localhost:8180/realms/my-test-realm/protocol/openid-connect/token',
+  tokenEndpoint:
+    'http://localhost:8180/realms/my-test-realm/protocol/openid-connect/token',
   redirectUri: window.location.origin,
   clientId: 'my-webapp-client',
   responseType: 'code',
@@ -18,13 +24,13 @@ function initializeOAuth(oauthService: OAuthService): Promise<void> {
   return new Promise((resolve) => {
     oauthService.configure(authCodeFlowConfig);
     oauthService.setupAutomaticSilentRefresh();
-    oauthService.loadDiscoveryDocumentAndLogin()
-      .then(() => resolve());
+    oauthService.loadDiscoveryDocumentAndLogin().then(() => resolve());
   });
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideRouter(routes),
     provideHttpClient(),
     provideOAuthClient(),
@@ -33,12 +39,10 @@ export const appConfig: ApplicationConfig = {
       useFactory: (oauthService: OAuthService) => {
         return () => {
           initializeOAuth(oauthService);
-        }
+        };
       },
       multi: true,
-      deps: [
-        OAuthService
-      ]
-    }
-  ]
+      deps: [OAuthService],
+    },
+  ],
 };
--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./views/products/products.component').then(
+        (m) => m.ProductsComponent
+      ),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./views/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./views/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'profil',
+    loadComponent: () =>
+      import('./views/profil/profil.component').then((m) => m.ProfilComponent),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import('./views/product-details/product-details.component').then(
+        (m) => m.ProductDetailsComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./views/notfound/notfound.component').then(
+        (m) => m.NotfoundComponent
+      ),
+  },
+];
